618e11f253eea26c7b9072b073a489e1e64861cc
AbidKhairyAK <abid.khairy.ak@gmail.com>
2022-08-25 17:13:45 +0700

    feat: change exam status from exam list + another minor improvement

--- src/views/lms/ujian-online/utama/Main.vue
+++ src/views/lms/ujian-online/utama/Main.vue
@@ -1,16 +1,24 @@
 <script setup>
 import { reactive, onMounted, ref, watch } from 'vue'
-import { request } from '@/util'
+import { useRouter } from 'vue-router';
 
+import { request } from '@/util'
 import ServerSideTable from '@/components/ServerSideTable.vue'
 import FilterSelect from '@/components/filter-select/index.vue'
+import Swal from 'sweetalert2';
+import qs from 'qs';
+import Spinner from '@/components/Spinner.vue';
+import { useToast } from 'vue-toast-notification';
 // import FormModal from './FormModal.vue'
 
+const router = useRouter()
 
 const tableRef = ref()
 const summaryData = ref({})
 const formMode = ref('')
 const activeData = ref({})
+const isSaving = ref(false)
+const isLoading = ref(false)
 
 const filterData = reactive({
 	options: {
@@ -51,27 +59,76 @@ function getTableData(payload) {
 	})
 }
 
-function getSummaryData() {
-	request.post('dashboard').then(res => {
-		summaryData.value = res.data.data
-	})
+async function getSummaryData() {
+	const res = await request.post('v2dev/summary')
+	summaryData.value = res.data.data
 }
 
-function getFilterOptions() {
-	Promise.all([
+async function getFilterOptions() {
+	const responses = await Promise.all([
 		request.post('mapel'),
 		request.post('user?level=guru'),
-	]).then(responses => {
-		filterData.options = {
-			mapel: responses[0].data.data,
-			user: responses[1].data.data
-		}
-	})
+	])
+
+	filterData.options = {
+		mapel: responses[0].data.data,
+		user: responses[1].data.data
+	}
 }
 
-function handleEdit(row) {
-	activeData.value = row
-	formMode.value = 'Edit Data'
+async function handleEditExam(row) {
+	if (row.exam_status == 1) return Swal.fire(
+		'Ujian telah aktif!',
+		'Ujian yang telah aktif tidak boleh diedit. Silahkan nonaktifkan ujian terlebih dahulu!',
+		'warning')
+
+	const invalidExamDate = Date.now() > new Date(row.exam_start_date).getTime()
+	if (invalidExamDate) return Swal.fire(
+		'Tanggal ujian telah lewat!',
+		'Ujian yang telah berlalu tidak boleh diedit. Anda dapat melakukan remedial jika ingin menduplikasi ujian.',
+		'warning')
+
+	const confirmEditing = await Swal.fire({
+		title: 'Tetap Edit?',
+		text: 'Soal mungkin perlu diurutkan kembali jika anda mengedit ujian.',
+		icon: 'question',
+		showCancelButton: true,
+		cancelButtonText: 'Batal',
+		confirmButtonText: 'Edit Ujian'
+	})
+
+	if (confirmEditing.isConfirmed) router.push('utama/form/pengaturan/' + row.exam_id)
+}
+
+async function changeExamStatus (row, status) {
+	const invalidExamDate = Date.now() > new Date(row.exam_start_date).getTime()
+	if (invalidExamDate) return Swal.fire(
+		'Tanggal ujian telah lewat!',
+		'Status ujian yang telah berlalu tidak boleh diubah',
+		'warning')
+
+	const confirmTitle = status ? 'Tetap Aktifkan?' : 'Tetap Nonaktifkan?'
+	const confirmText = status
+		? 'Ujian yang aktif dapat dilihat oleh siswa, dan tidak dapat diedit lagi hingga dinonaktifkan'
+		: 'Ujian yang nonaktif tidak dapat dilihat oleh siswa'
+	const confirmPopup = await Swal.fire(confirmTitle, confirmText, 'question')
+
+	if (!confirmPopup.isConfirmed) return
+
+	try {
+		isSaving.value = row.originalIndex
+
+		await request.post('exam/update-status', qs.stringify({
+			exam_id: row.exam_id,
+			user_id: row.user_id,
+			exam_status_new: status,
+		}))
+
+		tableData.rows[row.originalIndex].exam_status = status
+		useToast().info('Status ujian berhasil diperbarui!')
+	} finally {
+		isSaving.value = false
+	}
 }
 
 function handleFormClose(row) {
@@ -86,9 +143,16 @@ watch(
 	{ deep: true }
 )
 
-onMounted(() => {
-	getSummaryData()
-	getFilterOptions()
+onMounted(async () => {
+	try {
+		isLoading.value = true
+		await Promise.all([
+			getSummaryData(),
+			getFilterOptions()
+		])
+	} finally {
+		isLoading.value = false
+	}
 })
 </script>
 
@@ -96,33 +160,41 @@ onMounted(() => {
 	<div>
 		<div class="card mb-4">
 			<div class="card-body">
-				<div class="row px-5 gy-5">
+				<div v-if="isLoading" class="d-flex justify-content-center">
+					<Spinner />
+				</div>
+
+				<div v-else class="row">
 					<div class="col-md-3">
-						<section class="border-dashed p-4 rounded-2 w-75 mx-auto">
-							<p class="display-6">96</p>
+						<section class="summary-item bg-success bg-opacity-10 p-4 rounded-2">
+							<p class="display-6 text-success">{{ summaryData.exam_count }}</p>
 							<div class="h5">Ujian</div>
-							<div class="text-muted small">Online</div>
+							<div class="text-dark small">Online</div>
+							<i class="summary-icon text-success uil uil-clipboard-notes opacity-25"></i>
 						</section>
 					</div>
 					<div class="col-md-3">
-						<section class="border-dashed p-4 rounded-2 w-75 mx-auto">
-							<p class="display-6">96</p>
+						<section class="summary-item bg-primary bg-opacity-10 p-4 rounded-2">
+							<p class="display-6 text-primary">{{ summaryData.tugas_count }}</p>
 							<div class="h5">Tugas</div>
-							<div class="text-muted small">Online</div>
+							<div class="text-dark small">Online</div>
+							<i class="summary-icon text-primary uil uil-file-alt opacity-25"></i>
 						</section>
 					</div>
 					<div class="col-md-3">
-						<section class="border-dashed p-4 rounded-2 w-75 mx-auto">
-							<p class="display-6">96</p>
+						<section class="summary-item bg-warning bg-opacity-10 p-4 rounded-2">
+							<p class="display-6 text-warning">{{ summaryData.question_count }}</p>
 							<div class="h5">Soal Ujian</div>
-							<div class="text-muted small">Online</div>
+							<div class="text-dark small">Online</div>
+							<i class="summary-icon text-warning uil uil-comment-alt-notes opacity-25"></i>
 						</section>
 					</div>
 					<div class="col-md-3">
-						<section class="border-dashed p-4 rounded-2 w-75 mx-auto">
-							<p class="display-6">96</p>
+						<section class="summary-item bg-danger bg-opacity-10 p-4 rounded-2">
+							<p class="display-6 text-danger">{{ summaryData.guru_count }}</p>
 							<div class="h5">Guru Pembuat Ujian</div>
-							<div class="text-muted small">Online</div>
+							<div class="text-dark small">Online</div>
+							<i class="summary-icon text-danger uil uil-user-md opacity-25"></i>
 						</section>
 					</div>
 				</div>
@@ -131,7 +203,11 @@ onMounted(() => {
 
 		<div class="card mb-5 mb-xxl-8">
 			<div class="card-body">
-				<section class="d-flex flex-wrap justify-content-between align-items-center mb-5">
+				<div v-if="isLoading" class="d-flex justify-content-center">
+					<Spinner />
+				</div>
+
+				<section v-else class="d-flex flex-wrap justify-content-between align-items-center mb-5">
 					<section class="d-flex flex-wrap gap-4 align-items-center">
 						<FilterSelect v-model:filterValue="filterData.selected.mapel" placeholder="Pilih Mapel" class="w-150px">
 							<el-option
@@ -162,6 +238,7 @@ onMounted(() => {
 						Tambah Ujian
 					</router-link>
 				</section>
+
 				<ServerSideTable
 					ref="tableRef"
 					:totalRows="tableData.totalRows || 0"
@@ -187,26 +264,59 @@ onMounted(() => {
 							</span>
 						</div>
 						<div v-if="column.field == 'action'" class="d-flex justify-content-end">
-							<button v-if="!row.exam_parent" title="Remedial" class="btn btn-icon btn-bg-light btn-active-color-warning btn-sm me-2">
-								<span class="svg-icon svg-icon-3">
-									<inline-svg src="media/icons/duotune/arrows/arr029.svg" />
-								</span>
+							<div v-if="isSaving === row.originalIndex" class="h-30px d-flex align-items-center">
+								<Spinner />
+							</div>
+							<button
+								v-if="isSaving !== row.originalIndex && row.exam_status == 1"
+								class="btn btn-light btn-active-color-danger btn-sm me-2"
+								@click="changeExamStatus(row, 0)">
+								Nonaktifkan
 							</button>
-							<button title="Atur Ulang Waktu" class="btn btn-icon btn-bg-light btn-active-color-success btn-sm me-2">
-								<span class="svg-icon svg-icon-3">
-									<inline-svg src="media/icons/duotune/general/gen012.svg" />
-								</span>
+							<button
+								v-if="isSaving !== row.originalIndex && row.exam_status == 0"
+								class="btn btn-light btn-active-color-success btn-sm me-2"
+								@click="changeExamStatus(row, 1)">
+								Aktifkan
 							</button>
-							<router-link :to="'utama/form/pengaturan/' + row.exam_id" title="Edit" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-2">
-								<span class="svg-icon svg-icon-3">
-									<inline-svg src="media/icons/duotune/art/art005.svg" />
-								</span>
-							</router-link>
-							<button title="Hapus" class="btn btn-icon btn-bg-light btn-active-color-danger btn-sm">
-								<span class="svg-icon svg-icon-3">
-									<inline-svg src="media/icons/duotune/general/gen027.svg" />
-								</span>
-							</button>
+
+							<el-tooltip
+								class="box-item"
+								effect="light"
+								content="Edit Ujian"
+								placement="top-end">
+								<button
+									class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-2"
+									@click="handleEditExam(row)">
+									<span class="svg-icon svg-icon-3">
+										<inline-svg src="media/icons/duotune/art/art005.svg" />
+									</span>
+								</button>
+							</el-tooltip>
+
+							<el-dropdown size="large">
+								<button class="btn btn-icon btn-bg-light btn-active-color-success btn-sm">
+									<span class="svg-icon svg-icon-3">
+										<inline-svg src="media/icons/duotune/general/gen053.svg" />
+									</span>
+								</button>
+								<template #dropdown>
+									<el-dropdown-menu>
+										<el-dropdown-item>
+											<i class="uil uil-clock me-4 fs-3"></i>
+											Atur Ulang Waktu
+										</el-dropdown-item>
+										<el-dropdown-item>
+											<i class="uil uil-redo me-4 fs-3"></i>
+											Remedial
+										</el-dropdown-item>
+										<el-dropdown-item>
+											<i class="uil uil-trash-alt me-4 fs-3"></i>
+											Hapus
+										</el-dropdown-item>
+									</el-dropdown-menu>
+								</template>
+							</el-dropdown>
 						</div>
 					</template>
 				</ServerSideTable>
@@ -218,5 +328,15 @@ onMounted(() => {
 <style scoped>
 .border-dashed {
 	border: 2px dashed #000;
+}
+.summary-item {
+	position: relative;
+}
+.summary-icon {
+	position: absolute;
+	right: 16px;
+	top: 50%;
+	transform: translateY(-50%);
+	font-size: 84px;
 }
 </style>
