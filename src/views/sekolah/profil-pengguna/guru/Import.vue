3488b1a755b7ff1fe4b1a09d60c73e8222ed7004
gomgomgit <thechocohead@gmail.com>
2022-09-01 18:49:08 +0700

    import

--- src/views/sekolah/profil-pengguna/guru/Import.vue
+++ src/views/sekolah/profil-pengguna/guru/Import.vue
@@ -81,7 +81,7 @@ function generateData() {
 
     const data = XLSX.utils.aoa_to_sheet(items)
     const wb = XLSX.utils.book_new()
-    XLSX.utils.book_append_sheet(wb, data, 'guru')
+    XLSX.utils.book_append_sheet(wb, data, 'kelas')
     XLSX.writeFile(wb, name)
 }
 </script>
